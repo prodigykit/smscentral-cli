76e9b2a696785cfe99c347bab999e4872b800f0c
prodigykit <57336845+prodigykit@users.noreply.github.com>
2020-02-13 15:14:06 +1100

    request generation

--- sms.py
+++ sms.py
@@ -2,11 +2,14 @@
 # Licensed under the BSD-3 License.
 
 import profig
-from datetime import datetime.
+import requests
+from datetime import datetime
+
+# Globals
 now = datetime.now().strftime("%H:%M:%S")
+cfg = profig.Config('sms.cfg')
 
 # Setup config
-cfg = profig.Config('sms.cfg')
 cfg.init('api.url', 'https://my.smscentral.com.au/api/v3.2')
 cfg.init('api.username', None)
 cfg.init('api.password', None)
@@ -15,3 +18,25 @@ cfg.init('message.prefix', None)
 cfg.init('message.suffix', None)
 cfg.sync()
 # End Config
+
+# Function that sends API request
+def send_sms(recipient, message):
+    msg = """
+    %s
+    
+    %s
+    
+    %s
+    """ % (cfg['message.prefix'], message, cfg['message.suffix'])
+    payload = {
+        "USERNAME": cfg['api.username'],
+        "PASSWORD": cfg['api.username'],
+        "ACTION": "send",
+        "ORIGINATOR": "shared",
+        "RECIPIENT": recipient,
+        "CAMPAIGN": cfg['message.campaign'],
+        "MESSAGE_TEXT": msg
+    }
+    r = requests.post(cfg['api.url'],
+                      data=payload)
+    return str(r.text)
