8557129acbf1545b24d0bd264d74a2449d6aacda
prodigykit <57336845+prodigykit@users.noreply.github.com>
2020-02-13 15:39:21 +1100

    execution routine

--- sms.py
+++ sms.py
@@ -3,11 +3,14 @@
 
 import profig
 import requests
+import sys
+import argparse
 from datetime import datetime
 
 # Globals
 now = datetime.now().strftime("%H:%M:%S")
 cfg = profig.Config('sms.cfg')
+prsr = argparse.ArgumentParser(description="SMSCentral CLI Tool")
 
 # Setup config
 cfg.init('api.url', 'https://my.smscentral.com.au/api/v3.2')
@@ -18,6 +21,17 @@ cfg.init('message.prefix', None)
 cfg.init('message.suffix', None)
 cfg.sync()
 # End Config
+
+# Parser arguments
+prsr.add_argument('Message',
+                  metavar='message_text',
+                  type='str',
+                  help='Body of the message')
+prsr.add_argument('Recipient',
+                  metavar='recipient',
+                  type='str',
+                  help='Phone number of the recipient')
+#
 
 # Function that sends API request
 def send_sms(recipient, message):
@@ -40,3 +54,12 @@ def send_sms(recipient, message):
     r = requests.post(cfg['api.url'],
                       data=payload)
     return str(r.text)
+if __name__ == '__main__':
+    args = my_parser.parse_args()
+    res = send_sms(args.Recipient, args.Message)
+    if res == '0':
+        print("Message sent")
+        sys.exit(0)
+    else:
+        print("Error, API returned non-zero code")
+        sys.exit(1)
